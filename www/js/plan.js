ff28bb131678f29e194ce462b0d3f13e7dffef38
Chris Harris <chrisonefournine@hotmail.com>
2021-08-12 23:41:23 +0100

    More stuff done

--- www/js/plan.js
+++ www/js/plan.js
@@ -1,10 +1,25 @@
 const finalActions = document.getElementById('finalActions');
 const actions = JSON.parse(localStorage.getItem('listOfActions'));
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
 finalActions.innerHTML = actions
     .map(action => {
         return `<li class="action">${action}</li>`;
     })
     .join("");
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = "550px";
+      }
+    });
+  }
 
 // finalActions.innerText = actions;
